0a7100c0a68a2062910f21389c8c916b7d0f702a
SakirBey1 <sakirhack81@gmail.com>
2021-10-22 00:09:58 +0300

    Update debug.py

--- utils/debug.py
+++ utils/debug.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (C) @subinps
+# Copyright (C) @SakirBey1
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -37,7 +37,7 @@ from pyrogram.types import (
 from contextlib import suppress
 
 debug = Client(
-    "Debug",
+    "hata ayıklama",
     Config.API_ID,
     Config.API_HASH,
     bot_token=Config.BOT_TOKEN,
@@ -49,26 +49,26 @@ async def set_heroku_var(client, message):
     if message.from_user.id not in Config.SUDO:
         return await message.reply(f"/env command can only be used by creator of the bot, ({str(Config.SUDO)})")
     with suppress(MessageIdInvalid, MessageNotModified):
-        m = await message.reply("Checking config vars..")
+        m = await message.reply("Yapılandırma değişkenleri kontrol ediliyor...")
         if " " in message.text:
             cmd, env = message.text.split(" ", 1)
             if  not "=" in env:
-                await m.edit("You should specify the value for env.\nExample: /env CHAT=-100213658211")
+                await m.edit("env için değer belirtmelisiniz.\nÖrnek: /env Sohpet=-100213658211")
                 return
             var, value = env.split("=", 1)
         else:
-            await m.edit("You haven't provided any value for env, you should follow the correct format.\nExample: <code>/env CHAT=-1020202020202</code> to change or set CHAT var.\n<code>/env REPLY_MESSAGE= <code>To delete REPLY_MESSAGE.")
+            await m.edit("env için herhangi bir değer sağlamadınız, doğru formatı izlemelisiniz.\nÖrnek: <code>/env CHAT=-1020202020202</code> CHAT'ı değiştirmek veya ayarlamak için var.\n<code>/env REPLY_MESSAGE= <code>Silmek REPLY_MESSAGE.")
             return
 
         if Config.DATABASE_URI and var in ["STARTUP_STREAM", "CHAT", "LOG_GROUP", "REPLY_MESSAGE", "DELAY", "RECORDING_DUMP"]:      
-            await m.edit("Mongo DB Found, Setting up config vars...") 
+            await m.edit("Mongo DB Bulundu, Yapılandırma değişkenleri ayarlanıyor...") 
             if not value:
-                await m.edit(f"No value for env specified. Trying to delete env {var}.")
+                await m.edit(f"env için değer belirtilmedi. env'yi silmeye çalışıyorum {var}.")
                 if var in ["STARTUP_STREAM", "CHAT", "DELAY"]:
-                    await m.edit("This is a mandatory var and cannot be deleted.")
+                    await m.edit("Bu zorunlu bir değişkendir ve silinemez.")
                     return
                 await edit_config(var, False)
-                await m.edit(f"Sucessfully deleted {var}")
+                await m.edit(f"Başarıyla silindi {var}")
            
                 return
             else:
@@ -76,72 +76,72 @@ async def set_heroku_var(client, message):
                     try:
                         value=int(value)
                     except:
-                        await m.edit("You should give me a chat id . It should be an interger.")
+                        await m.edit("Bana bir sohbet kimliği vermelisin. Bir interger olmalı.")
         
                         return
                     if var == "CHAT":
                         Config.ADMIN_CACHE=False
                         Config.CHAT=int(value)
                     await edit_config(var, int(value))
-                    await m.edit(f"Succesfully changed {var} to {value}")
+                    await m.edit(f"Başarıyla değiştirildi {var} to {value}")
     
                     return
                 else:
                     if var == "STARTUP_STREAM":
                         Config.STREAM_SETUP=False
                     await edit_config(var, value)
-                    await m.edit(f"Succesfully changed {var} to {value}")
+                    await m.edit(f"Başarıyla değiştirildi {var} to {value}")
                     return
         else:
             if not Config.HEROKU_APP:
-                buttons = [[InlineKeyboardButton('Heroku API_KEY', url='https://dashboard.heroku.com/account/applications/authorizations/new'), InlineKeyboardButton('🗑 Close', callback_data='close'),]]
+                buttons = [[InlineKeyboardButton('Heroku API_KEY', url='https://dashboard.heroku.com/account/applications/authorizations/new'), InlineKeyboardButton('🗑 Kapat', callback_data='close'),]]
                 await m.edit(
                     text="No heroku app found, this command needs the following heroku vars to be set.\n\n1. <code>HEROKU_API_KEY</code>: Your heroku account api key.\n2. <code>HEROKU_APP_NAME</code>: Your heroku app name.", 
                     reply_markup=InlineKeyboardMarkup(buttons)) 
                 return     
             config = Config.HEROKU_APP.config()
             if not value:
-                await m.edit(f"No value for env specified. Trying to delete env {var}.")
+                await m.edit(f"env için değer belirtilmedi. env'yi silmeye çalışıyorum {var}.")
                 if var in ["STARTUP_STREAM", "CHAT", "DELAY", "API_ID", "API_HASH", "BOT_TOKEN", "SESSION_STRING", "ADMINS"]:
-                    await m.edit("These are mandatory vars and cannot be deleted.")
+                    await m.edit("Bunlar zorunlu değişkenlerdir ve silinemezler.")
     
                     return
                 if var in config:
-                    await m.edit(f"Sucessfully deleted {var}")
-                    await m.edit("Now restarting the app to make changes.")
+                    await m.edit(f"Başarıyla silindi {var}")
+                    await m.edit("Şimdi değişiklik yapmak için uygulamayı yeniden başlatıyoruz.")
                     if Config.DATABASE_URI:
                         msg = {"msg_id":m.message_id, "chat_id":m.chat.id}
-                        if not await db.is_saved("RESTART"):
-                            db.add_config("RESTART", msg)
+                        if not await db.is_saved("Tekrar başlat"):
+                            db.add_config("Tekrar başlat", msg)
                         else:
-                            await db.edit_config("RESTART", msg)
+                            await db.edit_config("Tekrar Başlat", msg)
                     del config[var]                
                     config[var] = None               
                 else:
-                    k = await m.edit(f"No env named {var} found. Nothing was changed.")
+                    k = await m.edit(f"env adlı bir ortam yok {var} bulundu. Hiçbir şey değişmedi.")
                 return
             if var in config:
-                await m.edit(f"Variable already found. Now edited to {value}")
+                await m.edit(f"Değişken zaten bulundu. Şimdi düzenlendi {value}")
             else:
-                await m.edit(f"Variable not found, Now setting as new var.")
-            await m.edit(f"Succesfully set {var} with value {value}, Now Restarting to take effect of changes...")
+                await m.edit(f"Değişken bulunamadı, Şimdi yeni değişken olarak ayarlanıyor.")
+            await m.edit(f"Başarıyla ayarlandı {var} değeri olan {value}, Şimdi değişikliklerin yürürlüğe girmesi için yeniden başlatılıyor...")
             if Config.DATABASE_URI:
                 msg = {"msg_id":m.message_id, "chat_id":m.chat.id}
-                if not await db.is_saved("RESTART"):
-                    db.add_config("RESTART", msg)
+                if not await db.is_saved("TEKRAR BAŞLAT"):
+                    db.add_config("TEKRAR BAŞLAT", msg)
                 else:
-                    await db.edit_config("RESTART", msg)
+                    await db.edit_config("TEKRAR BAŞLAT", msg)
             config[var] = str(value)
 
-@debug.on_message(filters.command(["restart", f"restart@{Config.BOT_USERNAME}"]) & filters.private & filters.user(Config.ADMINS))
+@debug.on_message(filters.command(["Tekrar Başlat", f"restart@{Config.BOT_USERNAME}"]) & filters.private & filters.user(Config.ADMINS))
 async def update(bot, message):
-    m=await message.reply("Restarting with new changes..")
+    m=await message.reply("Yeni değişikliklerle yeniden başlatılıyor...")
     if Config.DATABASE_URI:
         msg = {"msg_id":m.message_id, "chat_id":m.chat.id}
-        if not await db.is_saved("RESTART"):
-            db.add_config("RESTART", msg)
+        if not await db.is_saved("Tekrar Başlat"):
+            db.add_config("Tekrar Başlat", msg)
         else:
-            await db.edit_config("RESTART", msg)
+            await db.edit_config("Tekrar Başlat", msg)
     if Config.HEROKU_APP:
         Config.HEROKU_APP.restart()
     else:
@@ -149,21 +149,21 @@ async def update(bot, message):
             target=stop_and_restart()
             ).start()
 
-@debug.on_message(filters.command(["clearplaylist", f"clearplaylist@{Config.BOT_USERNAME}"]) & filters.private & filters.user(Config.ADMINS))
+@debug.on_message(filters.command(["temiz çalma listesi", f"clearplaylist@{Config.BOT_USERNAME}"]) & filters.private & filters.user(Config.ADMINS))
 async def clear_play_list(client, m: Message):
     if not Config.playlist:
-        k = await m.reply("Playlist is empty.")  
+        k = await m.reply("Oynatma listesi boş.")  
         return
     Config.playlist.clear()
-    k=await m.reply_text(f"Playlist Cleared.")
+    k=await m.reply_text(f"Oynatma Listesi Temizlendi.")
     await clear_db_playlist(all=True)
 
     
-@debug.on_message(filters.command(["skip", f"skip@{Config.BOT_USERNAME}"]) & filters.private & filters.user(Config.ADMINS))
+@debug.on_message(filters.command(["Atla", f"skip@{Config.BOT_USERNAME}"]) & filters.private & filters.user(Config.ADMINS))
 async def skip_track(_, m: Message):
-    msg=await m.reply('trying to skip from queue..')
+    msg=await m.reply('kuyruktan atlamaya çalışıyor..')
     if not Config.playlist:
-        await msg.edit("Playlist is Empty.")
+        await msg.edit("Oynatma Listesi Boş.")
         return
     if len(m.command) == 1:
         old_track = Config.playlist.pop(0)
@@ -176,50 +176,50 @@ async def skip_track(_, m: Message):
             items.sort(reverse=True)
             for i in items:
                 if 2 <= i <= (len(Config.playlist) - 1):
-                    await msg.edit(f"Succesfully Removed from Playlist- {i}. **{Config.playlist[i][1]}**")
+                    await msg.edit(f"Oynatma Listesinden Başarıyla Kaldırıldı- {i}. **{Config.playlist[i][1]}**")
                     await clear_db_playlist(song=Config.playlist[i])
                     Config.playlist.pop(i)
                 else:
-                    await msg.edit(f"You cant skip first two songs- {i}")
+                    await msg.edit(f"İlk iki şarkıyı atlayamazsın- {i}")
         except (ValueError, TypeError):
-            await msg.edit("Invalid input")
+            await msg.edit("Geçersiz Giriş")
     pl=await get_playlist_str()
     await msg.edit(pl, disable_web_page_preview=True)
 
 
-@debug.on_message(filters.command(['logs', f"logs@{Config.BOT_USERNAME}"]) & filters.private & filters.user(Config.ADMINS))
+@debug.on_message(filters.command(['Loglar', f"logs@{Config.BOT_USERNAME}"]) & filters.private & filters.user(Config.ADMINS))
 async def get_logs(client, message):
-    m=await message.reply("Checking logs..")
+    m=await message.reply("Günlükler kontrol ediliyor...")
     if os.path.exists("botlog.txt"):
         await message.reply_document('botlog.txt', caption="Bot Logs")
         await m.delete()
     else:
-        k = await m.edit("No log files found.")
+        k = await m.edit("Günlük dosyası bulunamadı.")
 
 @debug.on_message(filters.text & filters.private)
 async def reply_else(bot, message):
-    await message.reply(f"Development mode is activated.\nThis occures when there are some errors in startup of the bot.\nOnly Configuration commands works in development mode.\nAvailabe commands are /env, /skip, /clearplaylist and /restart and /logs\n\n**The cause for activation of development mode was**\n\n`{str(Config.STARTUP_ERROR)}`")
+    await message.reply(f"Geliştirme modu etkinleştirildi.\nBu, botun başlatılmasında bazı hatalar olduğunda ortaya çıkar.\nGeliştirme modunda yalnızca Yapılandırma komutları çalışır.\nKullanılabilir komutlar /env, /skip, /clearplaylist ve /restart ve /logs\n\n**Geliştirme modunun etkinleştirilmesinin nedeni şuydu:**\n\n`{str(Config.STARTUP_ERROR)}`")
 
 def stop_and_restart():
-    os.system("git pull")
+    os.system("git çek")
     time.sleep(10)
     os.execl(sys.executable, sys.executable, *sys.argv)
 
 async def get_playlist_str():
     if not Config.playlist:
-        pl = f"🔈 Playlist is empty.)ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ"
+        pl = f"🔈 Oynatma listesi boş.)ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ"
     else:
         if len(Config.playlist)>=25:
             tplaylist=Config.playlist[:25]
-            pl=f"Listing first 25 songs of total {len(Config.playlist)} songs.\n"
-            pl += f"▶️ **Playlist**: ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ\n" + "\n".join([
-                f"**{i}**. **🎸{x[1]}**\n   👤**Requested by:** {x[4]}"
+            pl=f"Toplam ilk 25 şarkı listeleniyor {len(Config.playlist)} Şarkılar.\n"
+            pl += f"▶️ **Oynatma Listesi**: ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ\n" + "\n".join([
+                f"**{i}**. **🎸{x[1]}**\n   👤**tarafından talep edildi:** {x[4]}"
                 for i, x in enumerate(tplaylist)
                 ])
             tplaylist.clear()
         else:
-            pl = f"▶️ **Playlist**: ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ\n" + "\n".join([
-                f"**{i}**. **🎸{x[1]}**\n   👤**Requested by:** {x[4]}\n"
+            pl = f"▶️ **Oynatma Listesi**: ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ\n" + "\n".join([
+                f"**{i}**. **🎸{x[1]}**\n   👤**tarafından talep edildi:** {x[4]}\n"
                 for i, x in enumerate(Config.playlist)
             ])
     return pl
